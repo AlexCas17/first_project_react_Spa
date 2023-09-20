1e2bc0ec86a6c7df0dbaae6397f32ff120c90bf6
Alexander Cardona Castro <alexandercardona2512@gmail.com>
2023-09-20 17:03:46 -0500

    Create a header and a Nav Bar

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,7 +1,26 @@
 import "./App.css";
 
+function Header() {
+  return <h1>Alex Spa</h1>;
+}
+
+function NavBar() {
+  return (
+    <nav>
+      <ul>
+        <li>Services</li>
+        <li>Calender</li>
+      </ul>
+    </nav>
+  );
+}
+
 function App() {
-  return <div className="App">hi</div>;
+  return (
+    <div className="App">
+      <NavBar />
+    </div>
+  );
 }
 
 export default App;
