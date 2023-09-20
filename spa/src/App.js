7e30fac70a29ed48e0277bdc0df6021cc4d5aea9
Alexander Cardona Castro <alexandercardona2512@gmail.com>
2023-09-20 17:18:04 -0500

    move header and nav to their own components

--- spa/src/App.js
+++ spa/src/App.js
@@ -1,24 +1,9 @@
 import "./App.css";
-
-function Header() {
-  return <h1>Alex Spa</h1>;
-}
-
-function NavBar() {
-  return (
-    <nav>
-      <ul>
-        <li>Services</li>
-        <li>Calender</li>
-      </ul>
-    </nav>
-  );
-}
-
+import Header from "./components/Header";
 function App() {
   return (
     <div className="App">
-      <NavBar />
+      <Header />
     </div>
   );
 }
